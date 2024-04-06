38fde24b18084da27e10b3c53362821919e37365
kunal-kumar-chaudhary <kkunal644@gmail.com>
2024-04-07 00:55:26 +0530

    create singup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -9,14 +10,15 @@ dotenv.config();
 mongoose.connect(process.env.MONGO);
 
 const app = express();
-console.log("point 1");
 
-app.use("/api/user", userRoutes);
-
-console.log("point 2");
-
-
+app.use(express.json());
 
 app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+    console.log("Server is running on http://localhost:3000");
+});  
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+
+
