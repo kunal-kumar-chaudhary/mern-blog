d332f6d0e79506384c977abc83a13e82ff52ba82
kunal-kumar-chaudhary <kkunal644@gmail.com>
2024-04-06 23:13:53 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,14 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO);
 
 const app = express();
 
-
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-    }
-);
+  console.log("Server is running on http://localhost:3000");
+});
