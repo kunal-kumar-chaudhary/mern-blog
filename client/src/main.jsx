552e10eec7624b3e7cbae52b331fbc11ca30bf38
kunal-kumar-chaudhary <kkunal644@gmail.com>
2024-04-08 01:26:58 +0530

    added redux persist (correction)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,9 +7,9 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <persistGate persistor={persistor}>
+  <PersistGate persistor={persistor}>
     <Provider store={store}>
       <App />
     </Provider>
-  </persistGate>
+  </PersistGate>
 );
