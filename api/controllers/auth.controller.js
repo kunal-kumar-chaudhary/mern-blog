e1893c59c7bd10e7dcb0cac6ffd890f76eb74886
kunal-kumar-chaudhary <kkunal644@gmail.com>
2024-04-07 11:38:55 +0530

    added functionality in the signup page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,16 +4,18 @@ import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
- 
+
   if (!username || !email || !password) {
     next(errorHandler(400, "All fields are required"));
   }
 
-  const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = await new User({ username, email, password: hashedPassword });
   try {
-
-    await newUser.save();
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
     res.json("Signup success");
   } catch (err) {
     next(err);
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,46 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Label, Button, TextInput } from "flowbite-react";
+import { Link, useNavigate } from "react-router-dom";
+import { Label, Button, TextInput, Spinner } from "flowbite-react";
+import { useState } from "react";
+import { Alert } from "flowbite-react";
 
 const SignUp = () => {
+    const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("All fields are required");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      // for server side error
+      if (data.sucess === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok){
+        navigate("/sign-in"); 
+      }
+    } catch (err) {
+      // for client side error
+      setErrorMessage(err.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -23,21 +61,47 @@ const SignUp = () => {
         </div>
         {/* right side */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label value="Your Username" />
-              <TextInput type="text" placeholder="username" id="username" />
+              <TextInput
+                type="text"
+                placeholder="username"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value="Your Email" />
-              <TextInput type="text" placeholder="Email" id="email" />
+              <TextInput
+                type="text"
+                placeholder="Email"
+                id="email"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value="Your Password" />
-              <TextInput type="text" placeholder="Password" id="password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
-            <Button gradientDuoTone="purpleToPink" type="submit">
-              SignUp
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign Up"
+              )} 
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
@@ -46,6 +110,11 @@ const SignUp = () => {
               Sign In
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
