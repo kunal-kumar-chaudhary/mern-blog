ffb9274a1712d0071358e25cb78c11d8a6f72082
kunal-kumar-chaudhary <kkunal644@gmail.com>
2024-04-09 01:34:56 +0530

    complete sidebar of the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
 
 const Dashboard = () => {
-  return (
-    <div>
-      <h1>dashboard</h1>
-    </div>
-  )
-}
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  console.log(location.search);
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
 
-export default Dashboard
+  // now after getting the tab from url, we can use it do render different components inside dashboard
+
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* sidebar */}
+        <DashSidebar />
+      </div>
+      <div className=" ">
+        {/* profile */}
+        {tab === "profile" && <DashProfile />}
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
