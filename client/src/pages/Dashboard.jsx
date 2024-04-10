c9e6fcb8a22c21405f66784dd7ed6ce804e10bb2
kunal-kumar-chaudhary <kkunal644@gmail.com>
2024-04-10 20:23:29 +0530

    add posts section to the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile";
+import DashPosts from "../components/DashPosts";
 
 const Dashboard = () => {
   const location = useLocation();
@@ -11,12 +12,13 @@ const Dashboard = () => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get("tab");
     if (tabFromUrl) {
-      setTab(tabFromUrl);
+      setTab(tabFromUrl); 
     }
   }, [location.search]);
 
   // now after getting the tab from url, we can use it do render different components inside dashboard
-
+  // when user will click posts or profile, we will change the url and get the tab from url
+  // and based on that we will render the component
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
       <div className="md:w-56">
@@ -25,6 +27,8 @@ const Dashboard = () => {
       </div>
         {/* profile */}
         {tab === "profile" && <DashProfile />}
+        {/* posts */}
+        {tab === "posts" && <DashPosts />}
     </div>
   );
 };
