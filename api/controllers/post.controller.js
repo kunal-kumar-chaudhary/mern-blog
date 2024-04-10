8fbc84f49a68fce987c3f48253769f02323584eb
kunal-kumar-chaudhary <kkunal644@gmail.com>
2024-04-10 16:29:20 +0530

    added create-post api route

---
+++ api/controllers/post.controller.js
@@ -0,0 +1,31 @@
+import { errorHandler } from "../utils/error.js";
+import Post from "../models/Post.js";
+
+export const create = async (req, res) => {
+
+    // if the uses is not admin, he/she is not allowed to create a post
+    if(!req.user.isAdmin){
+        return next(errorHandler(403, "you're not allowed to create a post"));
+    }
+
+    if(!req.body.title || !req.body.content)
+    {
+        return next(errorHandler(400, "All fields are required"));
+    }
+    
+    // slug will be used as url for directing to the post page
+    const slug = req.body.title.split(" ").join("-").toLowerCase().replace(/[^a-zA-Z0-9-]/g, "");
+
+    try{
+        const newPost = await Post.create({
+            ...req.body,
+            slug,   
+            userId: req.user.id,
+        });
+        res.status(201).json(newPost);
+    }
+    catch(err){
+        next(err);
+    }
+
+};
