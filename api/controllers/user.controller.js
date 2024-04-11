c122e22d0aca51a942d0da9d19314782e4ec1dd5
kunal-kumar-chaudhary <kkunal644@gmail.com>
2024-04-12 00:19:10 +0530

    create get users api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -78,3 +78,41 @@ export const signout = (req, res, next)=>{
         next(err);
     }
 };
+
+export const getusers = async (req, res, next)=>{
+    // if the user is not admin
+    if(!req.user.isAdmin){
+        return next(errorHandler(403, "you're not allowed to see all the users"));
+    }
+    try{
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.sort === "asc" ? 1: -1;
+        const users = await User.find().sort({createdAt: sortDirection}).
+        skip(startIndex).limit(limit);
+
+        const usersWithoutPassword = users.map((user)=>{
+            const {password, ...rest} = user._doc;
+            return res;
+        })
+
+        const totalUsers = await User.countDocuments();
+        const now = new Date();
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() - 1,
+            now.getDate(),
+        );
+
+        const lastMonthUsers = await User.countDocuments({
+            createdAt: {
+                $gte: oneMonthAgo
+            },
+        });
+
+        res.status(200).json({usersWithoutPassword, totalUsers, lastMonthUsers});
+
+    }catch(err){
+        next(err);
+    }
+}
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {updateUser, deleteUser, signout} from "../controllers/user.controller.js";
+import {updateUser, deleteUser, signout, getusers} from "../controllers/user.controller.js";
 import {verifyToken} from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
+router.get("/getusers", verifyToken, getusers);
 
 export default router; 
